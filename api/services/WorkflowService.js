64ad2075ae7b288767fd4e601c9b5beb93e2998b
John Negoita <johnny.php@gmail.com>
2017-03-29 09:16:41 +0300

    added toposort

--- api/services/WorkflowService.js
+++ api/services/WorkflowService.js
@@ -1,24 +1,13 @@
+var toposort = require('toposort');
+
 module.exports = {
 	createTasksHierarchy:function(flowDefinition){
-		this.discovered = [];
-		this.traverse(flowDefinition,0);
-		console.log(this.discovered);
-		return this.discovered;
-	},
-	traverse:function(flowDefinition,idx){
-		// 1  procedure DFS(G,v):
-		// 2      label v as discovered
-		// 3      for all edges from v to w in G.adjacentEdges(v) do
-		// 4          if vertex w is not labeled as discovered then
-		// 5              recursively call DFS(G,w)
-		var self = this;
-		this.discovered.push(idx);
+		var graph = [];
 		_.each(flowDefinition.links,function(link){
-			if (link.fromOperator == idx){
-				if (self.discovered.indexOf(link.toOperator) < 0){
-					self.traverse(flowDefinition,parseInt(link.toOperator));
-				}
-			} 
+			graph.push([link.fromOperator,link.toOperator]);
 		});
+
+		var executionList = toposort(graph).reverse();
+		console.log(executionList);
 	}
 }
